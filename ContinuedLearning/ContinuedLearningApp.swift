c1d576bf21bcccf53ace28c2437bcc009b470929
PeterZhu <zyxchelsea11@gmail.com>
2022-09-14 18:54:44 +0800

    Initial Commit

---
+++ ContinuedLearning/ContinuedLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContinuedLearningApp.swift
+//  ContinuedLearning
+//
+//  Created by 朱宇軒 on 2022/9/14.
+//
+
+import SwiftUI
+
+@main
+struct ContinuedLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
