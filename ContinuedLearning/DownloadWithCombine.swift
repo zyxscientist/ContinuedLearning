a41f6945b01a539d609454ff7c1f77f67f2231e6
PeterZhu <zyxchelsea11@gmail.com>
2022-10-16 17:04:54 +0800

    完成combine初步学习

---
+++ ContinuedLearning/DownloadWithCombine.swift
@@ -0,0 +1,80 @@
+//
+//  DownloadWithCombine.swift
+//  ContinuedLearning
+//
+//  Created by 朱宇軒 on 2022/10/16.
+//
+
+import SwiftUI
+import Combine
+
+struct ArticleModel: Codable, Identifiable {
+    let userId, id: Int
+    let title, body: String
+}
+
+class DownloadWithCombineViewModel: ObservableObject {
+    
+    @Published var article: [ArticleModel] = []
+    var cancellables = Set<AnyCancellable>()
+    
+    init(){
+        getArticle()
+    }
+    
+    func getArticle() {
+        
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
+        
+        // 使用Combine框架来做同样的事情
+        URLSession.shared.dataTaskPublisher(for: url)
+            // 表明接受后要在主线程更新
+            .receive(on: DispatchQueue.main)
+            // 检查错误，处理错误
+            .tryMap { (data, response) -> Data in
+                guard
+                    let response  = response as? HTTPURLResponse,
+                    response.statusCode >= 200 && response.statusCode < 300 else
+                {
+                    throw URLError(.badServerResponse)
+                }
+                return data
+            }
+            // 解码
+            .decode(type: [ArticleModel].self, decoder: JSONDecoder())
+            // 赋值
+            .sink { (completion) in
+                print("COMPLETION: \(completion)")
+            } receiveValue: { [weak self] (returnedArticle) in
+                self?.article = returnedArticle
+                print("APPEND TO ARTICLE")
+            }
+            // 不知道干嘛
+            .store(in: &cancellables)
+    }
+    
+}
+
+struct DownloadWithCombine: View {
+    
+    @StateObject var vm = DownloadWithCombineViewModel()
+    
+    var body: some View {
+        List{
+            ForEach(vm.article) { article in
+                LazyVStack(alignment: .leading) {
+                    Text(article.title)
+                        .font(.headline)
+                    Text(article.body)
+                        .foregroundColor(.gray)
+                }
+            }
+        }
+    }
+}
+
+struct DownloadWithCombine_Previews: PreviewProvider {
+    static var previews: some View {
+        DownloadWithCombine()
+    }
+}
