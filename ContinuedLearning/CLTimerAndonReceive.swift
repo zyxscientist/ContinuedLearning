8178ec4c9cf1c872e3f84f58f0b70011ac011ebe
PeterZhu <zyxchelsea11@gmail.com>
2022-10-18 21:49:11 +0800

    Timer 如何显示时间

---
+++ ContinuedLearning/CLTimerAndonReceive.swift
@@ -0,0 +1,52 @@
+//
+//  CLTimerAndonReceive.swift
+//  ContinuedLearning
+//
+//  Created by PeterZ on 2022/10/18.
+//
+
+import SwiftUI
+
+struct CLTimerAndonReceive: View {
+    
+    let timer = Timer.publish(every: 1.0, on: .main, in: .default).autoconnect()
+    
+    @State var currentDate: Date = Date()
+    
+    // 系统提供的一个渐变格式化器
+    var dateFormatter: DateFormatter {
+        let formatter = DateFormatter()
+        // formatter.dateStyle = .medium
+        formatter.timeStyle = .medium
+        return formatter
+    }
+    
+    
+    
+    var body: some View {
+        ZStack{
+            RadialGradient(colors: [Color.pink, Color.blue],
+                           center: .center,
+                           startRadius: 5,
+                           endRadius: 500)
+            .ignoresSafeArea()
+            
+            Text(dateFormatter.string(from: currentDate))
+                .foregroundColor(.white)
+                .font(.system(size: 100, weight: .bold, design: .monospaced))
+                .lineLimit(1)
+                .padding()
+                // 保证它总是一行显示完毕
+                .minimumScaleFactor(0.1)
+        }
+        .onReceive(timer, perform: { value in
+            currentDate = value
+        })
+    }
+}
+
+struct CLTimerAndonReceive_Previews: PreviewProvider {
+    static var previews: some View {
+        CLTimerAndonReceive()
+    }
+}
